df39fe02e4d4050f24fe08c98ec8818a110577be
Benoit Lamonica <ben33127@gmail.com>
2022-01-21 17:51:43 +0100

    test: button text

---
+++ src/routes/budgets/_components/LoadingText.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let test: string;
+</script>
+
+<p>Coucou {test}</p>
+
+<style lang="scss">
+</style>
---
+++ src/stories/LoadingText.stories.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf';
+  import LoadingText from '../routes/budgets/_components/LoadingText.svelte';
+</script>
+
+<Meta title="Text" component={LoadingText} argTypes={{}} />
+
+<Story name="Loading Text Component">
+  <LoadingText test="ksdjldskj" />
+</Story>
